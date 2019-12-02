336e6712c3c5a29605b86e689f954f6f94380520
OlexiyTelyakov <43723474+OlexiyTelyakov@users.noreply.github.com>
2019-12-02 11:53:58 +1000

    Add files via upload

---
+++ AI.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace KiwiKaleidoscope.AI
+{
+    /// <summary>
+    /// General base for non-Pest AI.
+    /// Handles a number of shared variables.
+    /// </summary>
+    public class AI : MonoBehaviour
+    {
+        [HideInInspector]
+        protected NavMeshAgent navAgent;
+        [HideInInspector]
+        protected Transform pilferTarget;
+        [HideInInspector]
+        protected Transform chaseTarget;
+
+        [Header("General AI")]
+        [SerializeField]
+        protected float maxIdleTime;
+        [SerializeField]
+        protected float minIdleTime;
+        [HideInInspector]
+        protected float idleTimer;
+        [SerializeField]
+        protected float maxWanderRange = 10f;
+        [SerializeField]
+        protected float minWanderRange = 4f;
+
+        [SerializeField]
+        protected float visionRadius;
+        [SerializeField]
+        protected int visionArc;
+
+        [Tooltip("Range for picking up items and such.")]
+        [SerializeField] protected float interactionRange;
+
+        protected List<Item> items = new List<Item>();
+
+        Vector3 destin;
+
+        protected BTState Plunder()
+        {
+            //If pilfer is null, state failed.
+            if (pilferTarget == null) return BTState.Failure;
+            //If item is in the interaction range, pick up the litter.
+            if (Vector3.Distance(pilferTarget.transform.position, transform.position) <= interactionRange)
+            {
+                PickUpItem();
+                return BTState.Success;
+            }
+            //Otherwise attempt to path the navAgent there and update the state as running.
+            else
+            {
+                if (navAgent.isStopped || !navAgent.hasPath)
+                {
+                    //Path towards the pilfer.
+                    navAgent.SetDestination(pilferTarget.position + (transform.position - pilferTarget.position).normalized * interactionRange * 0.9f);
+                    navAgent.isStopped = false;
+                }
+                return BTState.Running;
+            }
+        }
+
+        protected BTState Wander()
+        {
+            //If MooMoo is currently idling, return failure.
+            if (idleTimer != 0 && !navAgent.hasPath) return BTState.Failure;
+            if (!navAgent.hasPath)
+            {
+                //Reset idle timer.
+                idleTimer = Random.Range(minIdleTime, maxIdleTime);
+                //Find a new random spot to go to.
+                bool newPathGenerated = false;
+                while (!newPathGenerated)
+                {
+                    //Pick a random point on the navMesh.
+                    Vector3 dest = Random.insideUnitCircle.V2With().normalized * Random.Range(minWanderRange,maxWanderRange) + transform.position;
+                    NavMeshHit hit;
+                    //If it exists, go there.
+                    if(NavMesh.SamplePosition(dest, out hit, 1f, NavMesh.AllAreas))
+                    {
+                        navAgent.SetDestination(hit.position);
+                        destin = hit.position;
+                        navAgent.isStopped = false;
+                        break;
+                    }
+                }
+            }
+            return BTState.Running;
+        }
+
+        protected BTState Idle()
+        {
+            if (GameStateManager.CurrentGameState() != GameStateManager.GameState.InMenu)
+            {
+                //Increment the idle time
+                idleTimer -= Time.deltaTime;
+                idleTimer = Mathf.Clamp(idleTimer, 0, maxIdleTime);
+            }
+            //Idle is always running.
+            return BTState.Running;
+        }
+
+        /// <summary>
+        /// Resets the idle timer. Useful for higher priority states that might interrupt idling and not have it return to idle 0.245s.
+        /// Returns a failure so can be safely added to Selectors.
+        /// </summary>
+        /// <returns></returns>
+        protected BTState ResetIdle()
+        {
+            idleTimer = 0;
+            return BTState.Failure;
+        }
+
+        protected void PickUpItem()
+        {
+            //Get item reference.
+            PickUpItem litter = pilferTarget.GetComponent<PickUpItem>();
+            items.Add(litter.itemTemplate);
+            //Pocket it.
+            litter.Plunder();
+            //Reset pilfer target
+            pilferTarget = null;
+        }
+
+        private void OnDrawGizmos()
+        {
+                Gizmos.color = Color.white;
+                Gizmos.DrawSphere(destin, 0.25f);
+        }
+    }
+}
+
---
+++ BehaviorTree.cs
@@ -0,0 +1,175 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace KiwiKaleidoscope.AI
+{
+    /// <summary>
+    /// States a Behavior Tree node can return.
+    /// </summary>
+    public enum BTState
+    {
+        Success,
+        Failure,
+        Running
+    }
+
+    public static class BT
+    {
+        public static Selector Selector() { return new Selector(); }
+        public static Sequence Sequence() { return new Sequence(); }
+        public static Action Action(Func<BTState> action) { return new Action(action); }
+        public static Conditional If(Func<bool> condition) { return new Conditional(condition); }
+    }
+
+    /// <summary>
+    /// Behavior Tree base node implementation.
+    /// </summary>
+    public abstract class Node
+    {
+        //Node state getter.
+        protected BTState state;
+        public BTState State { get { return state; } }
+
+        /// <summary>
+        /// Update method for Behavior Tree nodes.
+        /// </summary>
+        /// <returns></returns>
+        public abstract BTState Tick();
+    }
+
+    public abstract class Block : Node
+    {
+        protected List<Node> nodes = new List<Node>();
+
+        public virtual Block AddNodes(params Node[] nodes)
+        {
+            for(int i = 0; i < nodes.Length; i++)
+            {
+                this.nodes.Add(nodes[i]);
+            }
+            return this;
+        }
+    }
+
+    /// <summary>
+    /// Run Tick on all children until one succeds and report success.
+    /// If all children fail, report failure.
+    /// </summary>
+    public class Selector : Block
+    {
+        //Update
+        public override BTState Tick()
+        {
+            //Selector returns true if any children return true and skips over failures.
+            for(int i = 0; i < nodes.Count; i++)
+            {
+                switch (nodes[i].Tick())
+                {
+                    case BTState.Success:
+                        return BTState.Success;
+                    case BTState.Failure:
+                        continue;
+                    case BTState.Running:
+                        return BTState.Running;
+                    default:
+                        continue;
+                }
+            }
+            //Base case is failure.
+            return BTState.Failure;
+        }
+    }
+
+    public class Sequence : Block
+    {
+        //Update
+        public override BTState Tick()
+        {
+            //Sequence returns fail if any nodes fail and success if all nodes are successful.
+            bool runningNodes = false;
+            for(int i = 0; i < nodes.Count; i++)
+            {
+                switch (nodes[i].Tick())
+                {
+                    case BTState.Success:
+                        continue;
+                    case BTState.Failure:
+                        return BTState.Failure;
+                    case BTState.Running:
+                        runningNodes = true;
+                        continue;
+                    default:
+                        return BTState.Failure;
+                }
+            }
+            //Base case is Success or Running if any nodes are running.
+            return runningNodes ? BTState.Running : BTState.Success;
+        }
+    }
+
+    public class Action : Node
+    {
+        protected Func<BTState> action;
+
+        //Constructor.
+        public Action(Func<BTState> action)
+        {
+            this.action = action;
+        }
+
+        public override BTState Tick()
+        {
+            //Actions fire off passed in logic and base their evaluation from that.
+            switch (action())
+            {
+                case BTState.Success:
+                    return BTState.Success;
+                case BTState.Failure:
+                    return BTState.Failure;
+                case BTState.Running:
+                    return BTState.Running;
+                default:
+                    return BTState.Failure;
+            }
+        }
+    }
+
+    /// <summary>
+    /// If boolean fails, reports failure. 
+    /// Otherwise runs Tick on all children, following Selector logic (run till 1 success).
+    /// </summary>
+    public class Conditional : Block
+    {
+        protected Func<bool> condition;
+
+        public Conditional(Func<bool> condition)
+        {
+            this.condition = condition;
+        }
+
+        public override BTState Tick()
+        {
+            //If condition isn't true, block is failed.
+            if (!condition()) return BTState.Failure;
+            //Otherwise it runs same logic to a selector.
+            for (int i = 0; i < nodes.Count; i++)
+            {
+                switch (nodes[i].Tick())
+                {
+                    case BTState.Success:
+                        return BTState.Success;
+                    case BTState.Failure:
+                        continue;
+                    case BTState.Running:
+                        return BTState.Running;
+                    default:
+                        continue;
+                }
+            }
+            //Base case is failure.
+            return BTState.Failure;
+        }
+    }
+}
